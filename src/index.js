25e7834c481992cde5216ef085127dd9bb050827
SaToGo1 <stoledano97@gmail.com>
2023-08-28 21:33:38 +0200

    Handle Error 404 and Error 500

--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ let app = express();
 
 let personRoute = require('./routes/person')
 
+let path = require('path')
+
+app.use((req, res, next) => {
+    console.log(`${new Date().toString()} => ${req.originalUrl}`)
+    next()
+})
 app.use(personRoute)
 app.use(express.static('public'));
 
+// Handler for 404 - Resource not found
+app.use((req, res, next) => {
+    res.status(404).send('We think you are lost!')
+})
+
+// Handler for Error 500
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+
+    res.sendFile(path.join(__dirname, '../public/500.html'))
+})
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`))
 
--- src/routes/person.js
+++ src/routes/person.js
@@ -1,14 +1,25 @@
 let express = require('express');
 let router = express.Router();
 
-// QueryString
+// QueryString => query property on the request object
+// localhost:3000/person?name=Sato&age=20
 router.get('/person', (req, res) => {
-    res.send('You have requested a person')
+    if(req.query.name) {
+        res.send(`You have requested a person ${req.query.name} by QueryString`)
+    }
+    else
+    {
+        res.send('You have requested a person')
+    }
 })
 
 // Params property on the request object
+// localhost:3000/person/Sato
 router.get('/person/:name', (req, res) => {
-    res.send(`You have requested a person ${req.params.name}`)
+    res.send(`You have requested a person ${req.params.name} by Params`)
 })
 
+router.get('/error', (req, res) => {
+    throw new Error('This is a forced error.');
+})
 module.exports = router
