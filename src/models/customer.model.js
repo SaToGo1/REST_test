dd113cdf7e1aec056960d530f97b69bfdd77514f
SaToGo1 <stoledano97@gmail.com>
2023-08-29 03:38:14 +0200

    Customer model mongoose

--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -1,5 +1,17 @@
+// NOT UPLOADED TO GITHUB
 let URI = require('../auth/auth');
+
 let mongoose = require('mongoose');
 
 mongoose.connect(URI)
 
+let CustomerSchema = new mongoose.Schema({
+    name: String,
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    }
+})
+
+module.exports = mongoose.model('Customer', CustomerSchema)
